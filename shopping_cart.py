a00a14d3cd0997460e40c81873ce72abcd9c5944
Sumedh <sumedhpanchadhar@gmail.com>
2019-12-23 11:12:54 -0500

    update readme

--- shopping_cart.py
+++ shopping_cart.py
@@ -1,50 +1,17 @@
 class ShoppingCart:
     # write your code here
-    
     def __init__(self, emp_discount=None):
-       self.total = 0
-       self.employee_discount = emp_discount
-       self.items = []
-    
-    
+      pass
     def add_item(self, name, price, quantity=1):
-        for i in list(range(quantity)):
-            self.items.append({"name": name, "price": price})
-            self.total += price
-        return self.total
-    
-    
+       pass
     def mean_item_price(self):
-        num_items = len(self.items)
-        total = self.total
-        mean = total/num_items
-        return mean
+       pass
 
-    
     def median_item_price(self):
-        prices = [item["price"] for item in self.items]
-        length = len(prices)
-        if (length%2 == 0):
-            mid_one = int(length/2)
-            mid_two = mid_one - 1
-            median = (prices[mid_one] + prices[mid_two])/2
-            return median
-        mid = int(length/2)
-        return prices[mid]
+        pass
 
-    
     def apply_discount(self):
-        if self.employee_discount:
-            discount = self.employee_discount/100
-            disc_total = self.total * (1 - discount)
-            return disc_total
-        else:
-            return "Sorry, there is no discount to apply to your cart :("
+       pass
 
-    
     def void_last_item(self):
-        if self.items:
-            removed_item = self.items.pop()
-        else:
-            return "There are no items in your cart!"
-        self.total -= removed_item['price']
+        pass
